ac85159eb1fb3e756ce9b6022158cf3f0850456f
llluns <55513956+llluns@users.noreply.github.com>
2019-11-20 15:43:47 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#Let's write our code here
+##Test the following functions using MAX 10 tweets at a time to avoid long run times 
+
+#write function to pull tweets with our specific parameters in mind
+
+
+#write function to create a dictionary with word frequencies 
+
+
+Etc....
+
+
+
+
+
+
+
+Goal is to access the twitter API and collect English tweets in the Ann Arbor area over a period of 3 days.
+ We’d create a dictionary of words from these tweets.
+We’d then collect 3 days of tweets 3 weeks later and identify words that increase in frequency.
+Those words would be labelled “slang”. (since we’re only using 2 time slices, we’d likely not include words that had 0 mentions in the first instance as a way to avoid an infinite frequency increase)
+Then we’d search through Urban Dictionary API to see if we can pull the definition of those slang words.
+We’d then display the 5 words with the largest slang  
