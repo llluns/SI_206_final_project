879f16e85a82d870762bae2c8021879187c48af2
shrinalipatel <55375523+shrinalipatel@users.noreply.github.com>
2019-12-10 20:15:08 -0500

    working code

--- football.py
+++ football.py
@@ -2,7 +2,7 @@ import requests
 import json
 import tweepy 			
 import sqlite3
-
+import matplotlib.pyplot as plt
 
 import sqlite3
 from sqlite3 import Error
@@ -16,45 +16,54 @@ def get_data():
     #print(json.dumps(results))
     return results
 
-def extract_info (results, week):
-    print(week)
 
+
+
+
+
+
+def input_week(week):
+    input_week = week
+    
     if week >=8 and week < 10:
         week = week-9
     elif week <=7 and week >=1:
         week = week + 3
+    elif week <=16 and week >11:
+        week = week -1
     else:
         week = week
+    #Checking and Showing which Weeks data we are looking at
+
+
+    print ("2019 College Football Rankings (Week:{})".format(input_week))
+
+
+    return week
+
+
+
+
+def extract_info_from_week():
 
 #Getting all data within inputted week
-
+    results=get_data()
+    week=input_week(4)
     week_data=results[week]
-    print("week data")
-    print(week_data)
 
 #Getting all the data within the polls part of the week
 
     all_poll_data=week_data['polls']
-    print(" All poll data_____________")
-    print(all_poll_data)
-    print("length")
-    print(len(all_poll_data))
-
+    num_of_polls_in_week = len(all_poll_data)
+    print("Polls:{}".format(num_of_polls_in_week))
+    
     giant_data = []
     count = 0
 
 #Looking at each individual poll inside the poll section of the week
     for i in range(0,len(all_poll_data)):
         count +=1
-        print("Poll Number:_________________" + str(count))
-
         polldata=all_poll_data[i]
-        print("Poll Data___________________:")
-
-        print(polldata)
-
-
-
 
 #itterating through each individual poll to extract and assign data values and also appending that data to a giant list
         if i == 0:
@@ -65,11 +74,7 @@ def extract_info (results, week):
                 rank = item["rank"]
                 conference = item["conference"]
                 id_name =  school + "_" + pollname
-
-                if id_name in giant_data:
-                    None
-                else:
-                    giant_data.append((id_name, school, rank, conference))
+                giant_data.append((id_name, school, rank, conference))
 
         if i == 1:
             pollname = polldata["poll"]
@@ -79,11 +84,7 @@ def extract_info (results, week):
                 rank = item["rank"]
                 conference = item["conference"]
                 id_name =  school + "_" + pollname
-
-                if id_name in giant_data:
-                    None
-                else:
-                    giant_data.append((id_name, school, rank, conference))
+                giant_data.append((id_name, school, rank, conference))
 
         if i == 2:
             pollname = polldata["poll"]
@@ -93,11 +94,7 @@ def extract_info (results, week):
                 rank = item["rank"]
                 conference = item["conference"]
                 id_name =  school + "_" + pollname
-
-                if id_name in giant_data:
-                    None
-                else:
-                    giant_data.append((id_name, school, rank, conference))
+                giant_data.append((id_name, school, rank, conference))
 
         if i == 3:
             pollname = polldata["poll"]
@@ -107,58 +104,226 @@ def extract_info (results, week):
                 rank = item["rank"]
                 conference = item["conference"]
                 id_name =  school + "_" + pollname
+                giant_data.append((id_name, school, rank, conference))
 
-                if id_name in giant_data:
-                    None
-                else:
-                    giant_data.append((id_name, school, rank, conference))
 
-    print("motherdata")
-    print(len(giant_data))
-    print(giant_data)
+        if i == 4:
+            pollname = polldata["poll"]
+
+            for item in polldata["ranks"]:
+                school = item["school"]
+                rank = item["rank"]
+                conference = item["conference"]
+                id_name =  school + "_" + pollname
+                giant_data.append((id_name, school, rank, conference))
+
+
+    # print("motherdata")
+    # print(len(giant_data))
+    # print(giant_data)
     return giant_data
 
-#Setting up table
+# Setting up table
 
 def set_football_db_one():
-    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/Football.sqlite')
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
     football=conn.cursor() 
     football.execute('DROP TABLE IF EXISTS Table1')
-    football.execute('CREATE TABLE Table1 (id_name TEXT, school TEXT, rank INTEGER)')
+    # football.execute('DROP TABLE IF EXISTS Table2')
 
+    football.execute('CREATE TABLE Table1 (id_name TEXT, school TEXT, rank INTEGER)')
+    # football.execute('CREATE TABLE Table2 (school TEXT, conference TEXT)')
 
 
 #Writing to Table 1
 
-
-def write_to_football_db():
-    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/Football.sqlite')
+def get_ids_from_Table2():
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
     football=conn.cursor() 
-    results = get_data()
-    giantdata = extract_info(results, 3)
     football.execute('SELECT id_name FROM Table1')
     currIds=[]      #fetch list of IDs
-    for Id in football:
-        currIds.append(Id)
-    count=0
-    while True:
-        for i in giantdata: 
+    try:
+        for Id in football:
+            currIds.append(Id)
+    except:
+        pass
+    return currIds
+
+
+def write_to_football_Table1():
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
+    football=conn.cursor() 
+
+    #currIds=get_ids_from_db()
+
+    giantdata = extract_info_from_week()
+    football.execute('CREATE TABLE IF NOT EXISTS Table1 (id_name TEXT, school TEXT, rank INTEGER)')
+    itemsInDB = football.execute("SELECT * FROM Table1")
+    itemsInDB = football.fetchall()
+    itemsInDB = len(itemsInDB)
+
+    if itemsInDB == 0:
+        for i in giantdata[:20]:
             id_name = i[0]
             school=i[1]
             rank=i[2]
-            if id_name in currIds:
-                pass
-            else:
-                football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
-                count+=1
-            if count ==20:
-                break
-    football.commit()
-    football.close()
+            football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
+
+    if itemsInDB == 20:
+        for i in giantdata[20:40]:
+            id_name = i[0]
+            school=i[1]
+            rank=i[2]
+            football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
+
+    if itemsInDB == 40:
+        for i in giantdata[40:60]:
+            id_name = i[0]
+            school=i[1]
+            rank=i[2]
+            football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
+    
+
+
+    if itemsInDB == 60:
+        for i in giantdata[60:80]:
+            id_name = i[0]
+            school=i[1]
+            rank=i[2]
+            football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
+    
+
+    if itemsInDB == 80:
+        for i in giantdata[80:100]:
+            id_name = i[0]
+            school=i[1]
+            rank=i[2]
+            football.execute('INSERT INTO Table1 (id_name, school, rank) VALUES (?, ?, ?)', (id_name, school, rank))
+    
+
+    print("Currently there are {} items in Table1".format(itemsInDB +20))
+
+    conn.commit()
+    conn.close()
+
+def get_avg_rates():
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
+    football=conn.cursor() 
+
+    results = football.execute("SELECT school, AVG(rank) FROM Table1 GROUP BY school")
+    results = football.fetchall()
+    school_and_rank = {}
+    for item in results:
+        school_and_rank[item[0]] = item[1]
+
+    return school_and_rank
+
+
+# def data_vis_Table1():
+#     data = get_avg_rates()
+#     ranks = list(data.values())
+#     schools = list(data.keys())
+#     lst=[]
+#     for i in range(0,len(ranks)):
+#         lst.append((schools[i],ranks[i]))
+#     sort=sorted(lst, key= lambda x: x[1], reverse=True)
+#     sorted_ranks=[]
+#     sorted_schools=[]
+#     for tup in sort:
+#         sorted_ranks.append(tup[1])
+#         sorted_schools.append(tup[0])
+
+
+#     fig = plt.figure()
+#     fig.subplots_adjust(top=0.8)
+#     ax1 = fig.add_subplot()
+
+
+#     plt.figure(1, figsize= (9, 3))
+#     plt.tight_layout()
+#     rainbow=["red","orange",'yellow','green','blue','purple']
+#     plt.barh(sorted_schools, sorted_ranks, color=rainbow)
+#     plt.suptitle('College Teams and Average Rankings in 2019')
+#     ax1.set_ylabel('College Teams')
+#     ax1.set_xlabel("Ranking")
+#     plt.show()
+#     plt.tight_layout()
+    
+
+def set_Table2():
+    giantdata = extract_info_from_week()
+
+    schools_and_conf = []
+    table2_data = []
+    for x in giantdata:
+        school = x[1]
+        conf = x[3]
+        schools_and_conf.append((school,conf))
+
+    for x in schools_and_conf:
+        if x in table2_data:
+            None
+        else:
+            table2_data.append(x)
+
+    return table2_data
+
+
+def write_to_football_Table2():
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
+    football=conn.cursor() 
+
+    
+
+    data_tups = set_Table2()
+    football.execute('CREATE TABLE IF NOT EXISTS Table2 (school TEXT, conference TEXT)')
+    itemsInDB2 = football.execute("SELECT * FROM Table2")
+    itemsInDB2 = football.fetchall()
+    itemsInDB2 = len(itemsInDB2)
+
+
+
+    if itemsInDB2 == 0:
+        for i in data_tups[:20]:
+            school=i[0]
+            conference=i[1]
+            football.execute('INSERT INTO Table2 (school, conference) VALUES (?, ?)', (school, conference))
 
 
 
 
+    if itemsInDB2 == 20:
+        for i in data_tups[20:40]:
+            school=i[0]
+            conference=i[1]
+            football.execute('INSERT INTO Table2 (school, conference) VALUES (?, ?)', (school, conference))
+
+
+
+    if itemsInDB2 == 40:
+        for i in data_tups[40:60]:
+            school=i[0]
+            conference=i[1]
+            football.execute('INSERT INTO Table2 (school, conference) VALUES (?, ?)', (school, conference))
+
+
+    if itemsInDB2 == 60:
+        for i in data_tups[60:80]:
+            school=i[0]
+            conference=i[1]
+            football.execute('INSERT INTO Table2 (school, conference) VALUES (?, ?)', (school, conference))
+
+    if itemsInDB2 == 80:
+        for i in data_tups[80:100]:
+            school=i[0]
+            conference=i[1]
+            football.execute('INSERT INTO Table2 (school, conference) VALUES (?, ?)', (school, conference))
+
+
+
+    print("Currently there are {} items in Table2".format(itemsInDB2 + 20))
+    conn.commit()
+    conn.close()
 
 
 
@@ -170,12 +335,18 @@ def write_to_football_db():
 
 
 def main():
-    yasss=get_data()
-    alldata=(extract_info(yasss, week=3))
+    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/collegefootball.sqlite')
+
+    get_data()
+    input_week(4)
+    extract_info_from_week()
+    write_to_football_Table1()
+    get_avg_rates()
+    
+    # data_vis_Table1()
+    set_Table2()
+    write_to_football_Table2()
+    print("______________________code end___________________")
+main()
 
 
-    conn=sqlite3.connect('/Users/shrinalipatel/Desktop/Football.sqlite')
-
-
-set_football_db_one()
-write_to_football_db()
